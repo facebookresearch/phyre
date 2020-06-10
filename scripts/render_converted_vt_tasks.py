3d18fe4bc756db12dc70ae263a0e6c414c56ff57
Anton Bakhtin <7470747+akhti@users.noreply.github.com>
2020-06-10 09:08:14 -0700

    Move things around

--- scripts/render_converted_vt_tasks.py
+++ scripts/render_converted_vt_tasks.py
@@ -1,8 +1,9 @@
 import os
 from phyre.simulator import scene_to_raster
-from phyre.creator import vt_converter
+import phyre.virtual_tools as vt_converter
 import json
 import phyre
+import phyre.settings
 import matplotlib.pyplot as plt
 
 PHYRE_SCALE = vt_converter.PHYRE_SCALE
@@ -14,7 +15,7 @@ if __name__ == '__main__':
     'Launch_A':[100, 540, 50], 'Launch_B':[70, 540, 30], 'Prevention_A':[475, 355, 30],
     'Prevention_B':[475, 355, 30], 'SeeSaw':[320, 45, 35], 'Table_A':[405, 35, 30],
     'Table_B':[310, 55, 40], 'Unbox':[530, 500, 50]}#'Towers_B':[230, 550, 40], 'Towers_A':[325, 550, 40], 
-    json_dir = 'VirtualTools/Original/'
+    json_dir = str(VIRTUAL_TOOLS_DIR / 'Original/')
     if not os.path.exists('/phyre_images/'):
         os.makedirs('/phyre_images/')
     for tnm in actions.keys():
